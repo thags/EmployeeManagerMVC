98bedc73ad0f3a6ec56f52b7f225a27b3ae20928
Tyler <tyler.hagstrom@gmail.com>
2022-02-18 15:41:54 -0600

    Removed ErrorViewModel and references to it.

--- EmployeeManager/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace EmployeeManager.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
