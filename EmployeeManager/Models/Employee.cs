63dad6e2732c0e030d4352e83954fc985a3a25d1
Tyler <tyler.hagstrom@gmail.com>
2022-02-18 15:42:12 -0600

    Add Employee model

---
+++ EmployeeManager/Models/Employee.cs
@@ -0,0 +1,11 @@
+namespace EmployeeManager.Models;
+
+public class Employee
+{
+    public int Id {get; set;}
+
+    public int DepartmentId {get; set;}
+
+    public string? FName {get; set;}
+    public string? LName {get; set;}
+}
