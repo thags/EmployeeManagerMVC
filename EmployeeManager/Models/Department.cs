8747327c101d14fe984f4d97e5e7b1ce942c5b75
Tyler <tyler.hagstrom@gmail.com>
2022-02-21 11:45:17 -0600

    Add department model

--- EmployeeManager/Models/Department.cs
+++ EmployeeManager/Models/Department.cs
@@ -3,7 +3,7 @@ namespace EmployeeManager.Models
 {
     public class Department
     {
-        int Id { get; set; }
-        string Name { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
