9da9c42bb4e707975df147b1d588c7c93bd75c78
Tyler <tyler.hagstrom@gmail.com>
2022-02-27 18:24:27 -0600

    Trying to transfer the data correctly

--- EmployeeManager/Models/EmployeeList.cs
+++ EmployeeManager/Models/EmployeeList.cs
@@ -3,6 +3,6 @@ namespace EmployeeManager.Models
     public class EmployeeList
     {
         public List<Employee>? Employees {get;set;}
-        public Employee? Employee {get;set;}
+        public Employee Employee {get;set;}
     }
 }
