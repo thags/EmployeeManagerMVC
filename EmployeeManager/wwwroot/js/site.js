1af9243555780d9b65e33b43dba313b340ace637
Tyler <tyler.hagstrom@gmail.com>
2022-03-03 10:53:28 -0600

    Add PopulateForm method and button action

--- EmployeeManager/wwwroot/js/site.js
+++ EmployeeManager/wwwroot/js/site.js
@@ -11,3 +11,25 @@
         }
     });
 }
+
+function PopulateForm(i)
+{
+    $.ajax({
+        url: 'Home/PopulateForm',
+        type: 'GET',
+        data: {
+            id: i
+        },
+        dataType: 'json',
+        success: function (response){
+            $("#Employee_FName").val(response.FName);
+            $("#Employee_LName").val(response.LName);
+            $("#Employee_Id").val(response.Id);
+            $("#Employee_DepartmentId").val(response.DepartmentId);
+
+
+            $("#form-button").val("Update Employee");
+            $("#form-action").attr("action", "/Home/Update");
+        }
+    });
+}
