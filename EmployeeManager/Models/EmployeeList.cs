0e431115e10a8c1eed1336c24ea06bb44d5a1e86
Tyler <tyler.hagstrom@gmail.com>
2022-03-03 10:54:46 -0600

    Made list<employee> non nullable

--- EmployeeManager/Models/EmployeeList.cs
+++ EmployeeManager/Models/EmployeeList.cs
@@ -2,7 +2,7 @@ namespace EmployeeManager.Models
 {
     public class EmployeeList
     {
-        public List<Employee>? Employees {get;set;}
+        public List<Employee> Employees {get;set;}
         public Employee Employee {get;set;}
     }
 }
